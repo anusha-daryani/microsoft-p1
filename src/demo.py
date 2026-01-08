0a0231c1bd4e5dd9d26e7de46be25c892e157351
anusha-daryani <daryanianusha02@gmail.com>
2026-01-08 12:25:28 +0530

    Add demo script

---
+++ src/demo.py
@@ -0,0 +1,6 @@
+import numpy as np
+print("NumPy version:", np.__version__)
+import sklearn
+print("scikit-learn version:", sklearn.__version__)
+import tensorflow as tf
+print("TensorFlow version:", tf.__version__)
